d949da2239d72c19294125eaeac5625bd687c2fe
Niranjana Sathish Avilery <niranjana.sathish1997@gmail.com>
2022-10-09 12:11:56 -0400

    imported dependencies & Setting up state hooks and references

--- appqna/src/App.js
+++ appqna/src/App.js
@@ -1,6 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 
+import React, {useEffect,useState,useRef} from 'react'
+
+// Importing dependencies
+// 1. Importing Tensorflow JS
+import * as tf from "@tensorflow/tfjs";
+// 2. Importing QnA model from Tensorflow
+import * as qna from "@tensorflow-models/qna";
+// 3. Importing CSS 
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+// 4. 
+import Loader from "react-loader-spinner";
+// 5. 
+import { Fragment } from 'react';
+
+
+// Setting up state hooks and references
+const refpassage = useRef(null);
+const refq = useRef(null);
+
 function App() {
   return (
     <div className="App">
