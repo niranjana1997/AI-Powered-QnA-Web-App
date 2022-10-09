ab7c206390db03304089b674cbff38f79354fa72
Niranjana Sathish Avilery <niranjana.sathish1997@gmail.com>
2022-10-09 13:54:33 -0400

    Project updated

--- appqna/src/App.js
+++ appqna/src/App.js
@@ -1,45 +1,84 @@
-import logo from './logo.svg';
+import React, {useRef, useEffect, useState} from 'react';
 import './App.css';
-
-// 
-import React, {useEffect,useState,useRef} from 'react'
 
 // Importing dependencies
 // 1. Importing Tensorflow JS
 import * as tf from "@tensorflow/tfjs";
 // 2. Importing QnA model from Tensorflow
 import * as qna from "@tensorflow-models/qna";
-// 3. Importing CSS 
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-// 4. 
-import Loader from "react-loader-spinner";
-// 5. 
+
+import {Vortex} from "react-loader-spinner";
 import { Fragment } from 'react';
 
 
-// Setting up state hooks and references
-const refpassage = useRef(null);
-const refq = useRef(null);
+const App = () => {
 
-function App() {
+  // Setting up states and references
+  const refParagraph = useRef(null); 
+  const refQuestion = useRef(null);
+  const [answer, setAnswer] = useState(); 
+  const [model, setModel] = useState(null); 
+
+  // Loading Tensorflow model
+  const loadTFModel = async () => {
+    const loadedTFModel = await qna.load();
+    setModel(loadedTFModel); 
+    console.log('Message: Tensorflow Model loaded');
+  } 
+
+  // Handling Questions
+  const questionanswer = async (e) => {
+    // checks if the model is loaded and if the user enters 'return' key on the keyboard
+    if (model !== null && e.which === 13) {
+      console.log('Message: Question submitted');
+      // getting the question value
+      const question = refQuestion.current.value;
+      // getting the paragraph value
+      const paragraph = refParagraph.current.value;
+      
+      // uses BERT model to find answers
+      const answer = await model.findAnswers(question, paragraph);
+      setAnswer(answer); 
+      console.log(answer);
+    }  
+  }
+
+  // To run the function loadTensorflowModel
+  // [] runs the function loadTensorflowModel only once
+  useEffect(()=>{loadTFModel()}, [])
+
+  // 2. Setting up paragraph, question and answer area
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {model ==null ? 
+          // Checks if the model is not loaded
+          <div>
+            <div>
+              Loading...
+            </div>      
+            <Vortex
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}/>
+          </div> 
+          :  
+          // If the mode is loaded
+          <Fragment>
+            Paragraph:
+            <textarea ref={refParagraph} rows="30" cols="100"></textarea>
+            Question:
+            <input ref={refQuestion} onKeyPress={questionanswer} size="80"></input>
+            <br /> 
+            Answer:
+            {/* Ternary check.  If there are any ans, mapping through each one of those. */}
+            {answer ? answer.map((ans, idx) =><div><b>Answer {idx+1} - </b> {answer.text} ({Math.floor(answer.score*100)/100})</div>) : ""}
+          </Fragment>
+        }
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
